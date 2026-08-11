89b1c66cdaeca3b349cd470fdbe942077f2733c0
Ordinary-Ladess <unferium@gmail.com>
2026-08-11 12:14:19 +0100

    Update buildme.sh

--- buildme.sh
+++ buildme.sh
@@ -2,6 +2,6 @@
 
 #Not sure what version to pull into lvgl, v8.x seem too old? v9.1 has fewest errors, so not sure if configs or something?
 #But future me: skip file if ./lvgl/README.md exists
-git clone -b release/v9.1 https://github.com/lvgl/lvgl.git lvgl
+git clone -b release/v9.5 https://github.com/lvgl/lvgl.git lvgl
 meson setup _build
 meson compile -C _build
